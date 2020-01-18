8fae995e1e349125d466ba2180019bf6d06744a5
Dom-888 <spinelladomenico88@gmail.com>
2020-01-18 11:41:41 +0000

    Complete first challenge

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -11,9 +11,44 @@ describe("What Can I drink function", function() {
     });
 
     describe("Checks Age", function() {
-        it("should return drink toddy when called as whatCanIDrink(13)", function() {
+        it("should return drink toddy when called as DrinkAbout(13)", function() {
             var result = DrinkAbout(13)
             expect(result).toBe("Drink Toddy");
+        });
+    });
+
+    describe("Checks Age", function() {
+        it("should return drink coke when called as DrinkAbout(17)", function() {
+            var result = DrinkAbout(17)
+            expect(result).toBe("Drink Coke");
+        });
+    });
+
+    describe("Checks Age", function() {
+        it("should return drink beer when called as DrinkAbout(20)", function() {
+            var result = DrinkAbout(20)
+            expect(result).toBe("Drink Beer");
+        });
+    });
+
+    describe("Checks Age", function() {
+        it("should return drink whisky when called as DrinkAbout(120)", function() {
+            var result = DrinkAbout(120)
+            expect(result).toBe("Drink Whisky");
+        });
+    });
+
+    describe("Checks Age", function() {
+        it("should return an error when called as DrinkAbout(-1)", function() {
+            var result = DrinkAbout(-1)
+            expect(result).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        });
+    });
+
+    describe("Checks Age", function() {
+        it("should return an error when called as DrinkAbout(131)", function() {
+            var result = DrinkAbout(131)
+            expect(result).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
         });
     });
 });
